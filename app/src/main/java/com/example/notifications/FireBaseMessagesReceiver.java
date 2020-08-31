ce0ac29c0b10a771bb8452c2c1cd22502d0c99d1
barakg <barakgonen9@gmail.com>
2020-08-31 08:18:49 +0300

    configuration files

---
+++ app/src/main/java/com/example/notifications/FireBaseMessagesReceiver.java
@@ -0,0 +1,39 @@
+package com.example.notifications;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import static android.content.ContentValues.TAG;
+
+public class FireBaseMessagesReceiver extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // ...
+        super.onMessageReceived(remoteMessage);
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                System.out.println("HI");
+            } else {
+                // Handle message within 10 seconds
+                System.out.println("BYE");
+            }
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+}
--- app/src/main/java/com/example/notifications/MainActivity.java
+++ app/src/main/java/com/example/notifications/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.notifications;
 
 import android.app.NotificationManager;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
+import static android.content.ContentValues.TAG;
 
 import java.util.HashMap;
 import java.util.Random;
@@ -23,6 +30,11 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     NotificationsHandler notificationsHandler;
     TextView txt;
+    private EditText emailTxt;
+    private EditText passwordTex;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,25 +47,106 @@ public class MainActivity extends AppCompatActivity {
         levelToMsg.put(NotificationManager.IMPORTANCE_HIGH, "HIGH-IMPORTANCE");
         levelToMsg.put(NotificationManager.IMPORTANCE_MAX, "MAX-IMPORTANCE");
 
-
         notificationsHandler = new NotificationsHandler(levelToMsg,
-                                               "myChannelID",
-                                            "myChannelName",
-                                             "channelDescription",
-                                                 this);
-        txt = findViewById(R.id.textViewToken);
-        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                "myChannelID",
+                "myChannelName",
+                "channelDescription",
+                this);
+//        txt = findViewById(R.id.tx);
+        mAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+        passwordTex = findViewById(R.id.passwordEditText);
+        emailTxt = findViewById(R.id.mailEditText);
+        findViewById(R.id.signUpBtn).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<InstanceIdResult> task) {
-                if (task.isSuccessful()){
-                    String token = task.getResult().getToken();
-                    txt.setText("Token:" + token);
-                } else{
-                    txt.setText(task.getException().toString());
-                }
+            public void onClick(View view) {
+                createUser();
             }
         });
 
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if (task.isSuccessful()) {
+                    String token = task.getResult().getToken();
+//                   bb txt.setText("Token:" + token);
+                } else {
+//                    txt.setText(task.getException().toString());
+                }
+            }
+        });
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+//                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, "TOKEN IS:" + token);
+                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void createUser() {
+        final String email = emailTxt.getText().toString().trim();
+        final String password = passwordTex.getText().toString().trim();
+        if (email.isEmpty()) {
+            emailTxt.setError("Email is required!");
+            emailTxt.requestFocus();
+        } else if (password.isEmpty() || password.length() < 6) {
+            passwordTex.setError("Password is required! and must be 6 chars or more");
+            passwordTex.requestFocus();
+        } else {
+            progressBar.setVisibility(View.VISIBLE);
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        startProfiledActivity();
+                    } else {
+                        if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                            userLogin(email, password);
+                        } else {
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Toast.
+                                    makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    }
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+            });
+        }
+    }
+
+    private void startProfiledActivity() {
+        Intent intent = new Intent(this, ProfiledActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    private void userLogin(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isComplete()) {
+                    startProfiledActivity();
+                } else {
+                    progressBar.setVisibility(View.INVISIBLE);
+                    Toast.
+                            makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG)
+                            .show();
+                }
+            }
+        });
     }
 
     private void displayNotification() {
